cba17558e9b1384b4d7e86c85993b4006a74d45d
Edward Omopariola <femiedjr@gmail.com>
2024-07-27 15:40:46 -0400

    function that joins an array together and seperated them with comma and space

---
+++ script.js
@@ -0,0 +1,13 @@
+/* Define a function that takes in an array of elements and joins them together into a string separated by a comma 
+   with a space after the comma.
+*/
+
+ let carsArr = ["bmw", "lexus", "nissan","toyota", "mercedes benz"]; // Array of cars with a strings inside of it.
+
+ function myFunction(vehicle) { // I define my function with a vehicle inside the parameters 
+    let carsArr = ["bmw", "lexus", "nissan","toyota", "mercedes benz"]; // I call back my array
+ }
+ 
+    console.log(carsArr.join(", ")) // I used the join method to return the spring follow by an empty string with a comma and a space
+
+

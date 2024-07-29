97e6e450df271a3390a4e4a8b1ab4f5ee6c6288b
Edward Omopariola <femiedjr@gmail.com>
2024-07-29 07:04:35 -0400

    changes on js

--- script.js
+++ script.js
@@ -3,11 +3,14 @@
 */
 
  let carsArr = ["bmw", "lexus", "nissan","toyota", "mercedes benz"]; // Array of cars with a strings inside of it.
-
- function myFunction(vehicle) { // I define my function with a vehicle inside the parameters 
-    let carsArr = ["bmw", "lexus", "nissan","toyota", "mercedes benz"]; // I call back my array
- }
  
-    console.log(carsArr.join(", ")) // I used the join method to return the spring follow by an empty string with a comma and a space
+function stringArray(vehicle) {
+    let carsArr = ["bmw", "lexus", "nissan","toyota", "mercedes benz"];
+    for(let i = 0; i < carsArr.length; i++){ // lopping through the carsArray
+        return vehicle.join(", ");
+    };
+};
+
+ console.log(stringArray(carsArr));
 
 
